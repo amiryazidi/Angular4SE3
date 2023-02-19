5a8fd5ca40d17fcf71bf4bad1116992f650233b1
Amir YAZIDI <amir.yazidi1@esprit.tn>
2023-02-19 13:12:29 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsProductComponent } from './details-product/details-product.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OffreEmploisComponent } from './offre-emplois/offre-emplois.component';
+import { ProdcutsComponent } from './prodcuts/prodcuts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'products', pathMatch:"full" },
+  {path:'products', component:ProdcutsComponent},
+  {path:'product/:id', component:DetailsProductComponent},
+  {path:'offres',component:OffreEmploisComponent},
+  {path:'**', component:NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdcutsComponent } from './prodcuts/prodcuts.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsProductComponent } from './details-product/details-product.component';
 
 
 @NgModule({
@@ -13,6 +16,9 @@ import { ProdcutsComponent } from './prodcuts/prodcuts.component';
     AppComponent,
     ProdcutsComponent,
     OffreEmploisComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    DetailsProductComponent,
 
   ],
   imports: [
